86a7e835ed30b712aef1baaea7b7141cb2f2219b
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-25 14:46:08 -0600

    test that the board list has a length of 9

--- tests/tic_tac_toe_test.py
+++ tests/tic_tac_toe_test.py
@@ -4,3 +4,4 @@ from tic_tac_toe.tic_tac_toe import TicTacToe
 def test_tic_tac_toe_board():
     ttt = TicTacToe()
     assert isinstance(ttt.board, list)  # board is a list
+    assert (len(ttt.board)) == 9        # board length is 9
