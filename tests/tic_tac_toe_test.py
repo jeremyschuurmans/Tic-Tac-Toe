71aa089fceb8416bd34eb6bf111fd78c7a8879c9
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-25 18:06:10 -0600

    add test and implementation for displaying board as a 3x3 grid

--- tests/tic_tac_toe_test.py
+++ tests/tic_tac_toe_test.py
@@ -3,6 +3,22 @@ from tic_tac_toe.tic_tac_toe import TicTacToe
 
 def test_tic_tac_toe_board():
     ttt = TicTacToe()
+    
     assert isinstance(ttt.board, list)  # board is a list
     assert (len(ttt.board)) == 9        # board length is 9
     assert ttt.board[0] == " "          # board contains empty strings
+
+def test_tic_tac_toe_display_board(capfd):
+    ttt = TicTacToe()
+
+    ttt.display_board(ttt.board)
+
+    mock_output = capfd.readouterr()
+    assert mock_output.out == '    |   |  \n' '--------------\n''    |   |  \n' '--------------\n' '    |   |  \n'
+        # this confusing-looking output of strings looks like this in the console:
+        #
+        #      |   |     
+        # ---------------
+        #      |   |     
+        # ---------------
+        #      |   |     
--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,3 +1,13 @@
 class TicTacToe:
     def __init__(self):
         self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    def display_board(self, board):
+        print(f'  {self.board[0]} | {self.board[1]} | {self.board[2]}')
+        print('--------------')
+        print(f'  {self.board[3]} | {self.board[4]} | {self.board[5]}')
+        print('--------------')
+        print(f'  {self.board[6]} | {self.board[7]} | {self.board[8]}')
+
+        # print(f'Hello {name}! This is {program}')
+    
