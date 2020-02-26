2c7406e5b7c21763c4d0bf19585cfb29ed276a8a
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-26 13:51:01 -0600

    Refactored tests and removed arg to display_board

--- tests/tic_tac_toe_test.py
+++ tests/tic_tac_toe_test.py
@@ -1,40 +1,43 @@
 import pytest
 from tic_tac_toe.tic_tac_toe import TicTacToe
 
-def test_tic_tac_toe_board():
-    ttt = TicTacToe(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-    
-    assert isinstance(ttt.board, list)  # board is a list
-    assert (len(ttt.board)) == 9        # board length is 9
-    assert ttt.board[0] == " "          # board contains empty strings
+
+def test_tic_tac_toe_initialized_with_a_3_x_3_board():
+    ttt = TicTacToe(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
+
+    assert len(ttt.board) == 9
+
 
 def test_tic_tac_toe_display_board(capfd):
-    ttt = TicTacToe(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    ttt = TicTacToe(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
 
-    ttt.display_board(ttt.board)
+    ttt.display_board()
 
     mock_output = capfd.readouterr()
-    assert mock_output.out == '    |   |  \n' '--------------\n''    |   |  \n' '--------------\n' '    |   |  \n'
-        # this confusing-looking output of strings looks like this in the console:
-        #
-        #      |   |     
-        # ---------------
-        #      |   |     
-        # ---------------
-        #      |   |     
+    expected = """\
+    |   |  
+--------------
+    |   |  
+--------------
+    |   |  
+"""
+    assert mock_output.out == expected
+
 
 def test_tic_tac_toe_display_board_with_user_token(capfd):
-    ttt = TicTacToe(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    ttt = TicTacToe(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
 
     ttt.board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
 
-    ttt.display_board(ttt.board)
+    ttt.display_board()
 
     mock_output_with_plays = capfd.readouterr()
-    assert mock_output_with_plays.out == '  X |   |  \n''--------------\n''    | X |  \n''--------------\n''    |   | X\n'
+    expected = """\
+  X |   |  
+--------------
+    | X |  
+--------------
+    |   | X
+"""
+    assert mock_output_with_plays.out == expected
 
-    #   X |   |
-    # --------------
-    #     | X |
-    # --------------
-    #     |   | X
