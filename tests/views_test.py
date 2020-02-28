86a133b4ccff05df8986b0452dec84cd949735f9
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-02-28 09:50:51 -0600

    add test for board output with plays present on board

--- tests/views_test.py
+++ tests/views_test.py
@@ -19,21 +19,19 @@ def test_tic_tac_toe_display_board(capfd):
     assert mock_output.out == expected
 
 
-# def test_tic_tac_toe_display_board_with_user_token(capfd):
-#     board_with_plays = Board()
+def test_tic_tac_toe_display_board_with_user_token(capfd):
 
-#     board_with_plays.board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+    board_with_plays = Board()
+    board_with_plays.board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+    board = BoardDisplay()
+    board.display_board(board_with_plays)
 
-#     board = BoardDisplay()
-
-#     board.display_board(board_with_plays)
-
-#     mock_output_with_plays = capfd.readouterr()
-#     expected = """\
-#   X |   |
-# --------------
-#     | X |
-# --------------
-#     |   | X
-# """
-#     assert mock_output_with_plays.out == expected
+    mock_output_with_plays = capfd.readouterr()
+    expected = """\
+  X |   |  
+--------------
+    | X |  
+--------------
+    |   | X
+"""
+    assert mock_output_with_plays.out == expected
