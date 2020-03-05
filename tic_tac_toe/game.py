9fb14d22131ffe18b3493f42765a13d5e9b9e8f2
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-05 12:00:24 -0600

    add basic game for testing

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -6,3 +6,17 @@ def run():
     board = Board()
     view = BoardDisplay()
     view.display_board(board)
+    turn_count = 0
+    while True:
+        print("Please select an open square by entering 1-9.")
+        if turn_count % 2 == 0:
+            board.move(selection=int(input()), token="X")
+            turn_count += 1
+            view.display_board(board)
+            print(f"Turn: {turn_count}")
+        else:
+            board.move(selection=int(input()), token="0")
+            turn_count += 1
+            view.display_board(board)
+            print(f"Turn: {turn_count}")
+
