406aca2ddb64f6e2fb4f22b3dca07a33d8b82ed7
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-05 12:00:00 -0600

    add turn_count, position_taken, and valid_move functions

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -6,4 +6,22 @@ class Board:
         return self.board[index]
 
     def move(self, selection, token="X"):
-        self.board[selection - 1] = token
+        if self.valid_move(selection - 1):
+            self.board[selection - 1] = token
+        else:
+            print("Oops, that selection was invalid.")
+
+    def turn_count(self):
+        return self.board.count("X") + self.board.count("O")
+
+    def position_taken(self, index):
+        if self.board[index] == "X" or self.board[index] == "O":
+            return True
+        else:
+            return False
+
+    def valid_move(self, index):
+        if index >= 0 and index <= 8 and self.position_taken(index) == False:
+            return True
+        else:
+            return False
