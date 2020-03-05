c69c40d930a429867d9d6badbca05db783263d52
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-05 11:58:38 -0600

    add tests for already occupied square and turn count function

--- tests/board_test.py
+++ tests/board_test.py
@@ -8,8 +8,36 @@ def test_tic_tac_toe_initialized_with_a_3_x_3_board():
 
 
 def test_tic_tac_toe_move():
-    fresh_board = Board()
+    new_board = Board()
 
-    fresh_board.move(selection=3)
+    new_board.move(selection=3)
 
-    assert fresh_board.board[2] == "X"
+    assert new_board.board[2] == "X"
+
+
+def test_player_cannot_place_token_on_square_already_occupied():
+    new_board = Board()
+
+    new_board.move(selection=5)
+
+    assert new_board.board[4] == "X"
+
+    new_board.move(selection=5, token="O")
+
+    assert new_board.board[4] == "X"
+
+
+def test_turn_count_keeps_track_of_number_of_turns():
+    new_board = Board()
+
+    new_board.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    assert new_board.turn_count() == 0
+
+    new_board.board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    assert new_board.turn_count() == 1
+
+    new_board.board = ["X", "O", " ", " ", " ", " ", " ", " ", " "]
+
+    assert new_board.turn_count() == 2
