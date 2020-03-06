11f19e2edbf9a99f5ee620ebb9c97d2a41fd9103
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-06 14:05:17 -0600

    add test for user input - TODO: debug

---
+++ tests/user_input_test.py
@@ -0,0 +1,7 @@
+# from tic_tac_toe.user_input import UserInput
+
+
+# def test_user_input_returns_input_as_integer_when_passed_a_number_string():
+#     user_input = UserInput()
+
+#     assert user_input.input_validator() ==
