b4b9f61cad4f0b82e64198f0f33d97694936a587
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-12 11:31:22 -0500

    fix broken tests

--- tests/errors_test.py
+++ tests/errors_test.py
@@ -8,7 +8,7 @@ def test_errors_can_print_input_not_numeric_error_message():
 
     error.input_not_numeric_error_message()
 
-    expected = "Sorry, that input is invalid. Please enter numbers 1 - 9:\n"
+    expected = "\nI didn't understand that ... trying entering a number: 1 - 9:\n"
 
     assert spy.printed() == expected
 
@@ -19,7 +19,7 @@ def test_errors_can_print_invalid_board_index_error_message():
 
     error.invalid_board_index_error_message()
 
-    expected = "Sorry, that input is out of range. Please enter 1 - 9:\n"
+    expected = "\nSorry, friend ... that number isn't one of the squares. Please enter 1 - 9:\n"
 
     assert spy.printed() == expected
 
@@ -30,7 +30,9 @@ def test_errors_can_print_position_already_taken_error_message():
 
     error.position_already_taken_error_message()
 
-    expected = "Sorry, that position has already been taken, please choose another:\n"
+    expected = (
+        "\nOops, that square already has a token on it, please choose another one:\n"
+    )
 
     assert spy.printed() == expected
 
